2652447a11175e3424b49f3c664c3471b9a8e2ca
Yaroslav Doroshchuk <iaroslav.doroshchuk@playtech.com>
2017-01-01 19:14:28 +0200

    add basic builder (es6) and make hello world app

---
+++ src/app/app.js
@@ -0,0 +1,26 @@
+import angular from 'angular';
+
+import '../style/app.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'http://localhost:8080/';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
